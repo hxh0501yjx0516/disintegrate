447a65d485e15e8755d63850667212592f8cbf9e
Leavonson <liwenxiang@chehulian.com>
2019-09-20 11:57:23 +0800

    更新

--- src/main/java/com/tieshan/disintegrate/controller/CarInformationController.java
+++ src/main/java/com/tieshan/disintegrate/controller/CarInformationController.java
@@ -2,6 +2,7 @@ package com.tieshan.disintegrate.controller;
 
 
 import com.tieshan.disintegrate.service.ICarInformationService;
+import com.tieshan.disintegrate.util.PubMethod;
 import com.tieshan.disintegrate.util.RestResult;
 import com.tieshan.disintegrate.util.ResultCode;
 import lombok.extern.apachecommons.CommonsLog;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description:查询单个车辆信息详情
+ * @description:车辆查询模块控制类
  * @author: Leavonson
  * @date: Created in 2019/9/18 14:51
  * @version: 1.0
@@ -26,24 +27,44 @@ public class CarInformationController {
     @Autowired
     private ICarInformationService iCarInformationService;
 
+    /**
+     *@Description: 车辆查询页面表头数据
+     * @param: carInfoId 车辆编号
+     * @return: RestResult
+     */
     @GetMapping("/findCarById")
-    public RestResult findCarById(@RequestParam("carInfoId") Integer carInfoId) {
-        RestResult restResult = new RestResult("车辆查询页面表头数据", iCarInformationService.findCarById(carInfoId),ResultCode.SUCCESS.code());
+    public RestResult findCarById(@RequestParam(value = "carInfoId", required = false) Long carInfoId) {
+        if (PubMethod.isEmpty(carInfoId)||carInfoId<1) {
+            return new RestResult("没有车辆编号", null, ResultCode.PARAM_IS_INVALID.code());
+        }
+        RestResult restResult = new RestResult(
+                "车辆查询页面表头数据",
+                iCarInformationService.findCarById(carInfoId),
+                ResultCode.SUCCESS.code());
         return  restResult;
     }
 
-    @GetMapping("/findCarInfoById")
-    public RestResult findCarInfoById(@RequestParam("carInfoId") Integer carInfoId) {
-        RestResult restResult = new RestResult("车辆信息查询", iCarInformationService.findCarInfoById(carInfoId),ResultCode.SUCCESS.code());
-        return  restResult;
-    }
-
+    /**
+     *@Description: 查询车辆信息，预处理、拓号 照片
+     * @param: carInfoId 车辆编号
+     * @param: dicType 来自字典
+     * @return: RestResult
+     */
     @GetMapping("/findCarPicById")
-    public RestResult findCarPicById(@RequestParam("carInfoId") Integer carInfoId) {
-        RestResult restResult = new RestResult("查询预处理、拓号 照片", iCarInformationService.findCarPicById(carInfoId),ResultCode.SUCCESS.code());
+    public RestResult findCarPicById(@RequestParam(value = "carInfoId", required = false) Long carInfoId) {
+
+        if (PubMethod.isEmpty(carInfoId)||carInfoId<1) {
+            return new RestResult("没有车辆编号", null, ResultCode.PARAM_IS_INVALID.code());
+        }
+        RestResult restResult = new RestResult("车辆信息，预处理、拓号 照片",iCarInformationService.findAll(carInfoId),ResultCode.SUCCESS.code());
         return  restResult;
     }
 
+    /**
+     *@Description: 查询车辆手续信息
+     * @param:
+     * @return:
+     */
     @GetMapping("/findCarSource")
     public RestResult findCarSource() {
         return null;
