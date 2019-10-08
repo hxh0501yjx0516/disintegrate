7d01b9b8a6178e1573b5a513532dd1a48c7b98b4
huxhua163@163.com <huxhua163@163.com>
2019-10-08 10:44:44 +0800

    系统日志，获取访问者ip--提交

--- src/main/java/com/tieshan/disintegrate/config/CustomInterceptor.java
+++ src/main/java/com/tieshan/disintegrate/config/CustomInterceptor.java
@@ -105,6 +105,7 @@ public class CustomInterceptor implements HandlerInterceptor {
             sysLog.setOperator(sysUser.getLogin_name());
             sysLog.setOperator_id(sysUser.getId());
             sysLog.setMethod_url(request.getRequestURL().toString());
+            sysLog.setIp_addr(getIpAddr(request));
             String param = getMonth(0);
             int num = sysUserMapper.existTable(param);
             if (num < 1) {
@@ -128,5 +129,35 @@ public class CustomInterceptor implements HandlerInterceptor {
         SimpleDateFormat sb = new SimpleDateFormat("yyyyMM");
         String retrunDate = sb.format(cal.getTime());
         return retrunDate;
+    }
+
+
+    private String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个ip值，第一个ip才是真实ip
+            if( ip.indexOf(",")!=-1 ){
+                ip = ip.split(",")[0];
+            }
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
