e4648ac4d7ef8b691d611fda9faf66a3ffd59797
huxhua163@163.com <huxhua163@163.com>
2019-10-17 16:48:02 +0800

    通知代码提交（初步版本）--提交

--- src/main/java/com/tieshan/disintegrate/controller/NoticeController.java
+++ src/main/java/com/tieshan/disintegrate/controller/NoticeController.java
@@ -51,23 +51,60 @@ public class NoticeController {
 
     }
 
+    /**
+     * 推送消息
+     *
+     * @param id
+     * @param request
+     * @return
+     */
     @PostMapping("/push")
-    public RestResult push(@RequestBody Notice notice, HttpServletRequest request) {
+    public RestResult push(String id, HttpServletRequest request) {
         RestResult restResult = null;
-        int num = noticeService.insertNotice(notice, request);
+        int num = noticeService.push(id, request);
         if (num > 0) {
             restResult = new RestResult("推送成功", null, ResultCode.SUCCESS.code());
-
         } else {
             restResult = new RestResult("推送失败", null, ResultCode.SUCCESS.code());
-
         }
-        restResult = new RestResult("推送成功", num, ResultCode.SUCCESS.code());
 
         return restResult;
 
     }
 
+    /**
+     * 插入数据库
+     *
+     * @param notice
+     * @param request
+     * @return
+     */
+    @PostMapping("/insertNotice")
+    public RestResult insertNotice(@RequestBody Notice notice, HttpServletRequest request) {
+        RestResult restResult = null;
+        int num = noticeService.insertNotice(notice, request);
+        if (num > 0) {
+            restResult = new RestResult("添加通知成功", null, ResultCode.SUCCESS.code());
+
+        } else {
+            restResult = new RestResult("添加通知失败", null, ResultCode.SUCCESS.code());
+
+        }
+
+        return restResult;
+
+    }
+
+    /**
+     * app消息列表
+     *
+     * @param type
+     * @param device_type
+     * @param request
+     * @param page
+     * @param pageSize
+     * @return
+     */
     @PostMapping("/selNotice")
     public RestResult selNotice(String type, String device_type, HttpServletRequest request,
                                 @RequestParam(value = "page", required = false, defaultValue = ConStants.PAGE) int page,
@@ -89,11 +126,69 @@ public class NoticeController {
         return restResult;
     }
 
+    /**
+     * 获取详情app
+     *
+     * @param id
+     * @return
+     */
     @PostMapping("/selNoticeById")
     public RestResult selNoticeById(String id) {
         RestResult restResult = null;
         try {
             Map<String, Object> resultMap = noticeService.selNoticeById(id);
+            if (!PubMethod.isEmpty(resultMap)) {
+                restResult = new RestResult("通知详情", resultMap, ResultCode.SUCCESS.code());
+            } else {
+                restResult = new RestResult("获取详情失败", resultMap, ResultCode.ERROR.code());
+
+            }
+        } catch (Exception e) {
+            log.info("获取详情失败---------->", e);
+        }
+        return restResult;
+    }
+
+    /**
+     * 获取消息列表  pc
+     *
+     * @param request
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @PostMapping("/selNoticePC")
+    public RestResult selNoticePC(HttpServletRequest request,
+                                  @RequestParam(value = "page", required = false, defaultValue = ConStants.PAGE) int page,
+                                  @RequestParam(value = "pageSize", required = false, defaultValue = ConStants.PAGESIZE) int pageSize) {
+        RestResult restResult = null;
+        try {
+            List<Map<String, Object>> resultList = noticeService.selNoticePC(request, page, pageSize);
+            PageInfo<Map<String, Object>> pageInfo = new PageInfo<>(resultList);
+
+            if (!PubMethod.isEmpty(resultList)) {
+                restResult = new RestResult("通知列表", pageInfo, ResultCode.SUCCESS.code());
+            } else {
+                restResult = new RestResult("暂无通知", null, ResultCode.ERROR.code());
+
+            }
+        } catch (Exception e) {
+            log.info("通知列表---------->", e);
+        }
+        return restResult;
+    }
+
+    /**
+     * 获取详情 pc
+     *
+     * @param id
+     * @return
+     */
+    @PostMapping("/selNoticeByIdPC")
+    public RestResult selNoticeByIdPC(String id) {
+        RestResult restResult = null;
+        try {
+            Map<String, Object> resultMap = noticeService.selNoticeByIdPC(id);
             if (!PubMethod.isEmpty(resultMap)) {
                 restResult = new RestResult("通知详情", resultMap, ResultCode.SUCCESS.code());
             } else {
@@ -104,5 +199,49 @@ public class NoticeController {
             log.info("通知详情---------->", e);
         }
         return restResult;
+    }
+
+    /**
+     * 删除通知 pc
+     *
+     * @param id
+     * @return
+     */
+    @PostMapping("/delNoticeById")
+    public RestResult delNoticeById(String id) {
+        RestResult restResult = null;
+        try {
+            int num = noticeService.delNoticeById(id);
+            if (num > 0) {
+                restResult = new RestResult("删除成功", null, ResultCode.SUCCESS.code());
+            } else {
+                restResult = new RestResult("删除失败", null, ResultCode.ERROR.code());
+
+            }
+        } catch (Exception e) {
+            log.info("删除通知---------->", e);
+        }
+        return restResult;
+    }
+
+    /**
+     * 更新通知
+     *
+     * @param notice
+     * @return
+     */
+    @PostMapping("/upNotieById")
+    public RestResult upNotieById(@RequestBody Notice notice) {
+        RestResult restResult = null;
+        int num = noticeService.upNotieById(notice);
+        if (num > 0) {
+            restResult = new RestResult("更新成功", null, ResultCode.SUCCESS.code());
+
+        } else {
+            restResult = new RestResult("更新失败", null, ResultCode.SUCCESS.code());
+
+        }
+        return restResult;
+
     }
 }
--- src/main/java/com/tieshan/disintegrate/service/INoticeService.java
+++ src/main/java/com/tieshan/disintegrate/service/INoticeService.java
@@ -14,13 +14,78 @@ import java.util.Map;
  * @modified By:
  */
 public interface INoticeService {
+    /**
+     * 获取头部 app
+     *
+     * @param request
+     * @return
+     */
     List<Map<String, Object>> getTop(HttpServletRequest request);
 
+    /**
+     * 推送 app
+     *
+     * @param id
+     * @param request
+     * @return
+     */
+    int push(String id, HttpServletRequest request);
+
+    /**
+     * 添加推送
+     *
+     * @param notice
+     * @param request
+     * @return
+     */
     int insertNotice(Notice notice, HttpServletRequest request);
 
-    List<Map<String, Object>> selNotice(String type, String device_type,HttpServletRequest request,int page, int pageSize);
+    /**
+     * 查询列表app
+     *
+     * @param type
+     * @param device_type
+     * @param request
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    List<Map<String, Object>> selNotice(String type, String device_type, HttpServletRequest request, int page, int pageSize);
 
+    /**
+     * 查询详情app
+     *
+     * @param id
+     * @return
+     */
     Map<String, Object> selNoticeById(String id);
+
+
+    List<Map<String, Object>> selNoticePC(HttpServletRequest request, int page, int pageSize);
+
+    /**
+     * 查询通知 pc
+     *
+     * @param id
+     * @return
+     */
+    Map<String, Object> selNoticeByIdPC(String id);
+
+    /**
+     * 删除通知
+     *
+     * @param id
+     * @return
+     */
+    int delNoticeById(String id);
+
+    /**
+     * 更新通知
+     *
+     * @param notice
+     * @return
+     */
+    int upNotieById(Notice notice);
 
 
 }
